4229140a0dd3e86ffe82d99d7ba2f28625c37a40
Jason Coposky <jasonc@renci.org>
2020-02-26 15:13:10 -0500

    add data replication

---
+++ libirods_rule_engine_plugin-policy_engine-data_replication.cpp
@@ -0,0 +1,209 @@
+
+#include "policy_engine.hpp"
+#include "filesystem.hpp"
+
+#include "irods_hierarchy_parser.hpp"
+#include "irods_server_api_call.hpp"
+#include "exec_as_user.hpp"
+
+#include "physPath.hpp"
+#include "apiNumber.h"
+
+namespace {
+    int replicate_object_to_resource(
+          rsComm_t*          _comm
+        , const std::string& _user_name
+        , const std::string& _object_path
+        , const std::string& _source_resource
+        , const std::string& _destination_resource)
+    {
+        dataObjInp_t data_obj_inp{};
+        rstrcpy(data_obj_inp.objPath, _object_path.c_str(), MAX_NAME_LEN);
+        data_obj_inp.createMode = getDefFileMode();
+        addKeyVal(&data_obj_inp.condInput, RESC_NAME_KW,      _source_resource.c_str());
+        addKeyVal(&data_obj_inp.condInput, DEST_RESC_NAME_KW, _destination_resource.c_str());
+
+        if(_comm->clientUser.authInfo.authFlag >= LOCAL_PRIV_USER_AUTH) {
+            addKeyVal(&data_obj_inp.condInput, ADMIN_KW, "true" );
+        }
+
+        transferStat_t* trans_stat{};
+
+        //const auto repl_err = irods::server_api_call(DATA_OBJ_REPL_AN, _comm, &data_obj_inp, &trans_stat);
+        auto repl_fcn = [&](auto& comm){
+            auto ret = irods::server_api_call(DATA_OBJ_REPL_AN, _comm, &data_obj_inp, &trans_stat);
+            free(trans_stat);
+            return ret;};
+
+        return irods::exec_as_user(*_comm, _user_name, repl_fcn);
+
+    } // replicate_object_to_resource
+
+    namespace pe = irods::policy_engine;
+
+    irods::error replication_policy(const pe::context ctx)
+    {
+        std::string user_name{}, object_path{}, source_resource{}, destination_resource{};
+
+        // query processor invocation
+        if(ctx.parameters.is_array()) {
+            using fsp = irods::experimental::filesystem::path;
+
+            std::string tmp_coll_name{}, tmp_data_name{};
+
+            std::tie(user_name, tmp_coll_name, tmp_data_name, source_resource) = irods::extract_array_parameters<4, std::string>(ctx.parameters);
+
+            object_path = (fsp{tmp_coll_name} / fsp{tmp_data_name}).string();
+        }
+        else {
+            // event handler or direct call invocation
+            std::tie(user_name, object_path, source_resource, destination_resource) =
+                irods::extract_dataobj_inp_parameters(
+                      ctx.parameters
+                    , irods::tag_first_resc);
+        }
+
+        auto comm = ctx.rei->rsComm;
+
+        if(!destination_resource.empty()) {
+
+            // direct call invocation
+            int err = replicate_object_to_resource(
+                            comm
+                          , user_name
+                          , object_path
+                          , source_resource
+                          , destination_resource);
+            if(err < 0) {
+                return ERROR(
+                          err,
+                          boost::format("failed to replicate [%s] from [%s] to [%s]")
+                          % object_path
+                          % source_resource
+                          % destination_resource);
+            }
+        }
+        else {
+            // event handler invocation
+            if(ctx.configuration.empty()) {
+                THROW(
+                    SYS_INVALID_INPUT_PARAM,
+                    boost::format("%s - destination_resource is empty and configuration is not provided")
+                    % ctx.policy_name);
+            }
+
+            destination_resource = irods::extract_object_parameter<std::string>("destination_resource", ctx.configuration);
+            if(!destination_resource.empty()) {
+                int err = replicate_object_to_resource(
+                                comm
+                              , user_name
+                              , object_path
+                              , source_resource
+                              , destination_resource);
+                if(err < 0) {
+                    return ERROR(
+                              err,
+                              boost::format("failed to replicate [%s] from [%s] to [%s]")
+                              % object_path
+                              % source_resource
+                              % destination_resource);
+                }
+            }
+            else {
+                auto src_dst_map{ctx.configuration.at("source_to_destination_map")};
+                if(src_dst_map.empty()) {
+                    THROW(
+                        SYS_INVALID_INPUT_PARAM,
+                        boost::format("%s - destination_resource or source_to_destination_map not provided")
+                        % ctx.policy_name);
+                }
+
+                auto dst_resc_arr{src_dst_map.at(source_resource)};
+                auto destination_resources = dst_resc_arr.get<std::vector<std::string>>();
+                irods::error ret{SUCCESS()};
+                for( auto& dest : destination_resources) {
+                    int err = replicate_object_to_resource(
+                                    comm
+                                  , user_name
+                                  , object_path
+                                  , source_resource
+                                  , dest);
+                    if(err < 0) {
+                        ret = PASSMSG(
+                                  boost::str(boost::format("failed to replicate [%s] from [%s] to [%s]")
+                                  % object_path
+                                  % source_resource),
+                                  ret);
+                    }
+                }
+
+                if(!ret.ok()) {
+                    return ret;
+                }
+            }
+        }
+
+        return SUCCESS();
+
+    } // replication_policy
+
+} // namespace
+
+const char usage[] = R"(
+{
+    "id": "file:///var/lib/irods/configuration_schemas/v3/policy_engine_usage.json",
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "description": ""
+        "input_interfaces": [
+            {
+                "name" : "event_handler-collection_modified",
+                "description" : "",
+                "json_schema" : ""
+            },
+            {
+                "name" :  "event_handler-data_object_modified",
+                "description" : "",
+                "json_schema" : ""
+            },
+            {
+                "name" :  "event_handler-metadata_modified",
+                "description" : "",
+                "json_schema" : ""
+            },
+            {
+                "name" :  "event_handler-user_modified",
+                "description" : "",
+                "json_schema" : ""
+            },
+            {
+                "name" :  "event_handler-resource_modified",
+                "description" : "",
+                "json_schema" : ""
+            },
+            {
+                "name" :  "direct_invocation",
+                "description" : "",
+                "json_schema" : ""
+            },
+            {
+                "name" :  "query_results"
+                "description" : "",
+                "json_schema" : ""
+            },
+        ],
+    "output_json_for_validation" : ""
+}
+)";
+
+
+extern "C"
+pe::plugin_pointer_type plugin_factory(
+      const std::string& _plugin_name
+    , const std::string&)
+{
+    return pe::make(
+             _plugin_name
+            , "irods_policy_data_replication"
+            , usage
+            , replication_policy);
+} // plugin_factory
---
+++ packaging/test_plugin_policy_engine-data_replication.py
@@ -0,0 +1,438 @@
+import os
+import sys
+import shutil
+import contextlib
+import tempfile
+import json
+import os.path
+
+from time import sleep
+
+if sys.version_info >= (2, 7):
+    import unittest
+else:
+    import unittest2 as unittest
+
+from ..configuration import IrodsConfig
+from ..controller import IrodsController
+from .resource_suite import ResourceBase
+from ..test.command import assert_command
+from . import session
+from .. import test
+from .. import paths
+from .. import lib
+import ustrings
+
+@contextlib.contextmanager
+def data_replication_configured(arg=None):
+    filename = paths.server_config_path()
+    with lib.file_backed_up(filename):
+        irods_config = IrodsConfig()
+        irods_config.server_config['advanced_settings']['rule_engine_server_sleep_time_in_seconds'] = 1
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+           {
+                "instance_name": "irods_rule_engine_plugin-policy_engine-data_replication-instance",
+                "plugin_name": "irods_rule_engine_plugin-policy_engine-data_replication",
+                "plugin_specific_configuration": {
+                    "log_errors" : "true"
+                }
+           }
+        )
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+           {
+                "instance_name": "irods_rule_engine_plugin-policy_engine-query_processor-instance",
+                "plugin_name": "irods_rule_engine_plugin-policy_engine-query_processor",
+                "plugin_specific_configuration": {
+                    "log_errors" : "true"
+                }
+           }
+        )
+
+        irods_config.commit(irods_config.server_config, irods_config.server_config_path)
+
+        IrodsController().restart()
+
+        try:
+            yield
+        finally:
+            pass
+
+@contextlib.contextmanager
+def data_replication_with_event_handler_configured(arg=None):
+    filename = paths.server_config_path()
+    with lib.file_backed_up(filename):
+        irods_config = IrodsConfig()
+        irods_config.server_config['advanced_settings']['rule_engine_server_sleep_time_in_seconds'] = 1
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+            {
+                "instance_name": "irods_rule_engine_plugin-event_handler-data_object_modified-instance",
+                "plugin_name": "irods_rule_engine_plugin-event_handler-data_object_modified",
+                "plugin_specific_configuration": {
+                    "policies_to_invoke" : [
+                        {    "pre_or_post_invocation" : ["post"],
+                            "events" : ["create", "read", "write", "rename", "registration"],
+                            "policy"    : "irods_policy_data_replication",
+                            "configuration" : {
+                                "destination_resource" : "AnotherResc"
+                            }
+                        }
+                    ]
+                }
+            }
+        )
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+           {
+                "instance_name": "irods_rule_engine_plugin-policy_engine-data_replication-instance",
+                "plugin_name": "irods_rule_engine_plugin-policy_engine-data_replication",
+                "plugin_specific_configuration": {
+                    "log_errors" : "true"
+                }
+           }
+        )
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+           {
+                "instance_name": "irods_rule_engine_plugin-policy_engine-query_processor-instance",
+                "plugin_name": "irods_rule_engine_plugin-policy_engine-query_processor",
+                "plugin_specific_configuration": {
+                    "log_errors" : "true"
+                }
+           }
+        )
+
+        irods_config.commit(irods_config.server_config, irods_config.server_config_path)
+
+        IrodsController().restart()
+
+        try:
+            yield
+        finally:
+            pass
+
+@contextlib.contextmanager
+def data_replication_alternate_attributes_configured(arg=None):
+    filename = paths.server_config_path()
+    with lib.file_backed_up(filename):
+        irods_config = IrodsConfig()
+        irods_config.server_config['advanced_settings']['rule_engine_server_sleep_time_in_seconds'] = 1
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+           {
+                "instance_name": "irods_rule_engine_plugin-policy_engine-data_replication-instance",
+                "plugin_name": "irods_rule_engine_plugin-policy_engine-data_replication",
+                "plugin_specific_configuration": {
+                    "log_errors" : "true",
+                    "attribute"  : "data_replication_attribute"
+                }
+           }
+        )
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+           {
+                "instance_name": "irods_rule_engine_plugin-policy_engine-query_processor-instance",
+                "plugin_name": "irods_rule_engine_plugin-policy_engine-query_processor",
+                "plugin_specific_configuration": {
+                    "log_errors" : "true"
+                }
+           }
+        )
+
+        irods_config.commit(irods_config.server_config, irods_config.server_config_path)
+
+        IrodsController().restart()
+
+        try:
+            yield
+        finally:
+            pass
+
+@contextlib.contextmanager
+def data_replication_alternate_attributes_with_event_handler_configured(arg=None):
+    filename = paths.server_config_path()
+    with lib.file_backed_up(filename):
+        irods_config = IrodsConfig()
+        irods_config.server_config['advanced_settings']['rule_engine_server_sleep_time_in_seconds'] = 1
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+            {
+                "instance_name": "irods_rule_engine_plugin-event_handler-data_object_modified-instance",
+                "plugin_name": "irods_rule_engine_plugin-event_handler-data_object_modified",
+                "plugin_specific_configuration": {
+                    "policies_to_invoke" : [
+                        {    "pre_or_post_invocation" : ["post"],
+                            "events" : ["create", "read", "write", "rename", "registration"],
+                            "policy"    : "irods_policy_data_replication",
+                            "configuration" : {
+                                "attribute"  : "event_handler_attribute",
+                                "destination_resource" : "AnotherResc"
+                            }
+                        }
+                    ]
+                }
+            }
+        )
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+           {
+                "instance_name": "irods_rule_engine_plugin-policy_engine-data_replication-instance",
+                "plugin_name": "irods_rule_engine_plugin-policy_engine-data_replication",
+                "plugin_specific_configuration": {
+                    "log_errors" : "true",
+                    "attribute"  : "data_replication_attribute"
+                }
+           }
+        )
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+           {
+                "instance_name": "irods_rule_engine_plugin-policy_engine-query_processor-instance",
+                "plugin_name": "irods_rule_engine_plugin-policy_engine-query_processor",
+                "plugin_specific_configuration": {
+                    "log_errors" : "true"
+                }
+           }
+        )
+
+        irods_config.commit(irods_config.server_config, irods_config.server_config_path)
+
+        IrodsController().restart()
+
+        try:
+            yield
+        finally:
+            pass
+
+
+class TestPolicyEngineDataReplication(ResourceBase, unittest.TestCase):
+    def setUp(self):
+        super(TestPolicyEngineDataReplication, self).setUp()
+
+    def tearDown(self):
+        super(TestPolicyEngineDataReplication, self).tearDown()
+
+
+
+    def test_direct_invocation(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            try:
+                filename = 'test_put_file'
+                lib.create_local_testfile(filename)
+                admin_session.assert_icommand('iput ' + filename)
+
+                rule = """
+{
+    "policy" : "irods_policy_execute_rule",
+    "payload" : {
+        "policy_to_invoke" : "irods_policy_data_replication",
+        "parameters" : {
+            "user_name" : "rods",
+            "object_path" : "/tempZone/home/rods/test_put_file",
+            "source_resource" : "demoResc",
+            "destination_resource" : "AnotherResc"
+        },
+        "configuration" : {
+        }
+    }
+}
+INPUT null
+OUTPUT ruleExecOut"""
+
+                rule_file = tempfile.NamedTemporaryFile(mode='wt', dir='/tmp', delete=False).name + '.r'
+                with open(rule_file, 'w') as f:
+                    f.write(rule)
+
+                with data_replication_configured():
+                    admin_session.assert_icommand(['irule', '-r', 'irods_rule_engine_plugin-cpp_default_policy-instance', '-F', rule_file])
+                    admin_session.assert_icommand('ils -l '+filename, 'STDOUT_SINGLELINE', 'AnotherResc')
+            finally:
+                admin_session.assert_icommand('irm -f ' + filename)
+
+
+
+    def test_direct_invocation_source_to_destination_map(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            try:
+                filename = 'test_put_file'
+                lib.create_local_testfile(filename)
+                admin_session.assert_icommand('iput ' + filename)
+
+                rule = """
+{
+    "policy" : "irods_policy_execute_rule",
+    "payload" : {
+        "policy_to_invoke" : "irods_policy_data_replication",
+        "parameters" : {
+            "user_name" : "rods",
+            "object_path" : "/tempZone/home/rods/test_put_file",
+            "source_resource" : "demoResc"
+        },
+        "configuration" : {
+            "source_to_destination_map" : {
+                "demoResc" : ["TestResc", "AnotherResc"]
+            }
+        }
+    }
+}
+INPUT null
+OUTPUT ruleExecOut"""
+
+                rule_file = tempfile.NamedTemporaryFile(mode='wt', dir='/tmp', delete=False).name + '.r'
+                with open(rule_file, 'w') as f:
+                    f.write(rule)
+
+                with data_replication_configured():
+                    admin_session.assert_icommand(['irule', '-r', 'irods_rule_engine_plugin-cpp_default_policy-instance', '-F', rule_file])
+                    admin_session.assert_icommand('ils -l '+filename, 'STDOUT_SINGLELINE', 'AnotherResc')
+                    admin_session.assert_icommand('ils -l '+filename, 'STDOUT_SINGLELINE', 'TestResc')
+            finally:
+                admin_session.assert_icommand('irm -f ' + filename)
+
+
+
+    def test_event_handler_invocation(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            with data_replication_with_event_handler_configured():
+                try:
+                    filename = 'test_put_file'
+                    lib.create_local_testfile(filename)
+                    admin_session.assert_icommand('iput ' + filename)
+                    admin_session.assert_icommand('ils -l ' + filename, 'STDOUT_SINGLELINE', 'AnotherResc')
+                finally:
+                    admin_session.assert_icommand('irm -f ' + filename)
+
+
+
+    def test_query_invocation(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            try:
+                filename = 'test_put_file'
+                lib.create_local_testfile(filename)
+                admin_session.assert_icommand('iput ' + filename)
+                admin_session.assert_icommand('ils -l', 'STDOUT_SINGLELINE', filename)
+                admin_session.assert_icommand('imeta ls -d ' + filename, 'STDOUT_SINGLELINE', 'None')
+
+                rule = """
+{
+    "policy" : "irods_policy_execute_rule",
+    "payload" : {
+        "policy_to_invoke" : "irods_policy_query_processor",
+        "parameters" : {
+              "query_string" : "SELECT USER_NAME, COLL_NAME, DATA_NAME, RESC_NAME WHERE COLL_NAME = '/tempZone/home/rods' AND DATA_NAME = 'test_put_file' AND RESC_NAME = 'demoResc'",
+              "query_limit" : 10,
+              "query_type" : "general",
+              "number_of_threads" : 1,
+              "policy_to_invoke" : "irods_policy_data_replication",
+              "configuration" : {
+                  "destination_resource" : "AnotherResc"
+              }
+         }
+    }
+}
+INPUT null
+OUTPUT ruleExecOut"""
+
+                rule_file = tempfile.NamedTemporaryFile(mode='wt', dir='/tmp', delete=False).name + '.r'
+                with open(rule_file, 'w') as f:
+                    f.write(rule)
+
+                with data_replication_configured():
+                    admin_session.assert_icommand(['irule', '-r', 'irods_rule_engine_plugin-cpp_default_policy-instance', '-F', rule_file])
+                    admin_session.assert_icommand('ils -l ' + filename, 'STDOUT_SINGLELINE', 'AnotherResc')
+            finally:
+                admin_session.assert_icommand('irm -f ' + filename)
+
+
+
+    def test_direct_invocation_alternate_attribute(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            try:
+                filename = 'test_put_file'
+                lib.create_local_testfile(filename)
+                admin_session.assert_icommand('iput ' + filename)
+
+                rule = """
+{
+    "policy" : "irods_policy_execute_rule",
+    "payload" : {
+        "policy_to_invoke" : "irods_policy_data_replication",
+        "parameters" : {
+            "user_name" : "rods",
+            "object_path" : "/tempZone/home/rods/test_put_file",
+            "source_resource" : "demoResc",
+            "destination_resource" : "AnotherResc"
+        },
+        "configuration" : {
+        }
+    }
+}
+INPUT null
+OUTPUT ruleExecOut"""
+
+                rule_file = tempfile.NamedTemporaryFile(mode='wt', dir='/tmp', delete=False).name + '.r'
+                with open(rule_file, 'w') as f:
+                    f.write(rule)
+
+                with data_replication_alternate_attributes_configured():
+                    admin_session.assert_icommand(['irule', '-r', 'irods_rule_engine_plugin-cpp_default_policy-instance', '-F', rule_file])
+                    admin_session.assert_icommand('ils -l '+filename, 'STDOUT_SINGLELINE', 'AnotherResc')
+            finally:
+                admin_session.assert_icommand('irm -f ' + filename)
+
+
+
+    def test_event_handler_invocation_alternate_attribute(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            with data_replication_alternate_attributes_with_event_handler_configured():
+                try:
+                    filename = 'test_put_file'
+                    lib.create_local_testfile(filename)
+                    admin_session.assert_icommand('iput ' + filename)
+                    admin_session.assert_icommand('ils -l '+filename, 'STDOUT_SINGLELINE', 'AnotherResc')
+                finally:
+                    admin_session.assert_icommand('irm -f ' + filename)
+
+
+
+    def test_query_invocation_alternate_attribute(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            try:
+                filename = 'test_put_file'
+                lib.create_local_testfile(filename)
+                admin_session.assert_icommand('iput ' + filename)
+                admin_session.assert_icommand('ils -l', 'STDOUT_SINGLELINE', filename)
+
+                rule = """
+{
+    "policy" : "irods_policy_execute_rule",
+    "payload" : {
+        "policy_to_invoke" : "irods_policy_query_processor",
+        "parameters" : {
+              "query_string" : "SELECT USER_NAME, COLL_NAME, DATA_NAME, RESC_NAME WHERE COLL_NAME = '/tempZone/home/rods' AND DATA_NAME = 'test_put_file' AND RESC_NAME = 'demoResc'",
+              "query_limit" : 10,
+              "query_type" : "general",
+              "number_of_threads" : 1,
+              "policy_to_invoke" : "irods_policy_data_replication",
+              "configuration" : {
+                  "attribute" : "query_processor_attribute",
+                  "destination_resource" : "AnotherResc"
+              }
+         }
+    }
+}
+INPUT null
+OUTPUT ruleExecOut"""
+
+                rule_file = tempfile.NamedTemporaryFile(mode='wt', dir='/tmp', delete=False).name + '.r'
+                with open(rule_file, 'w') as f:
+                    f.write(rule)
+
+                with data_replication_alternate_attributes_configured():
+                    admin_session.assert_icommand(['irule', '-r', 'irods_rule_engine_plugin-cpp_default_policy-instance', '-F', rule_file])
+                    admin_session.assert_icommand('ils -l '+filename, 'STDOUT_SINGLELINE', 'AnotherResc')
+            finally:
+                admin_session.assert_icommand('irm -f ' + filename)
+
