3f59230f45abd44a42b3127f8991dc38d84edf4e
Jason Coposky <jasonc@renci.org>
2020-12-07 16:59:31 -0500

    event handler resource modified

---
+++ libirods_rule_engine_plugin-event_handler-resource_modified.cpp
@@ -0,0 +1,20 @@
+#include "general_administration_handler.hpp"
+
+namespace {
+
+    auto user_handler(
+          const std::string&        _rule_name
+        , const ie::arguments_type& _arguments
+        , ruleExecInfo_t*           _rei) -> ie::handler_return_type
+    {
+        return general_administration_handler("resource", _rule_name, _arguments, _rei);
+    } // user_handler
+
+} // namespace
+
+extern "C"
+ie::plugin_pointer_type plugin_factory(const std::string& _pn, const std::string& _ctx)
+{
+    ie::register_handler("general_admin", ie::interfaces::api, user_handler);
+    return ie::make(_pn, _ctx);
+} // plugin_factory
