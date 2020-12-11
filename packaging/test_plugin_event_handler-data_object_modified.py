7f49e7d8d6af04ee181273a4a1fa7f54c752a2a8
Jason Coposky <jasonc@renci.org>
2020-12-11 15:58:11 -0500

    tests for metadata_exists conditional

--- packaging/test_plugin_event_handler-data_object_modified.py
+++ packaging/test_plugin_event_handler-data_object_modified.py
@@ -118,7 +118,7 @@ def event_handler_configured_fail_conditional(arg=None):
             pass
 
 @contextlib.contextmanager
-def event_handler_recurisve_collection_metadata(arg=None):
+def event_handler_recurisve_collection_metadata_exists(arg=None):
     filename = paths.server_config_path()
     with lib.file_backed_up(filename):
         irods_config = IrodsConfig()
@@ -132,17 +132,138 @@ def event_handler_recurisve_collection_metadata(arg=None):
                     "policies_to_invoke" : [
                         {
                             "conditional" : {
-                                "logical_path" : "\/tempZone.*"
-                                "metadata" : {
-                                    "entity_type" : "collection",
+
+                                "logical_path" : "\/tempZone.*",
+
+                                "metadata_exists" : {
                                     "recursive"   : "true",
-                                    "attribute"   : "",
-                                    "value"       : "",
-                                    "units"       : "",
+                                    "entity_type" : "collection",
+                                    "attribute"   : "test_attribute",
+                                    "value"       : "test_value",
+                                    "units"       : "test_units",
                                 }
                             },
                             "active_policy_clauses" : ["post"],
-                            "events" : ["put", "get", "create", "read", "write", "rename", "register", "unregister", "replication", "checksum", "copy", "seek", "truncate", "open", "close"],
+                            "events" : ["put", "get", "create", "read",
+                                        "write", "rename", "register", "unregister",
+                                        "replication", "checksum", "copy", "seek",
+                                        "truncate", "open", "close"],
+                            "policy"    : "irods_policy_testing_policy",
+                            "configuration" : {
+                            }
+                        }
+                    ]
+                }
+            }
+        )
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+           {
+                "instance_name": "irods_rule_engine_plugin-policy_engine-testing_policy-instance",
+                "plugin_name": "irods_rule_engine_plugin-policy_engine-testing_policy",
+                "plugin_specific_configuration": {
+                    "log_errors" : "true"
+                }
+           }
+        )
+
+        irods_config.commit(irods_config.server_config, irods_config.server_config_path)
+
+        IrodsController().restart()
+
+        try:
+            yield
+        finally:
+            pass
+
+@contextlib.contextmanager
+def event_handler_user_metadata_exists(arg=None):
+    filename = paths.server_config_path()
+    with lib.file_backed_up(filename):
+        irods_config = IrodsConfig()
+        irods_config.server_config['advanced_settings']['rule_engine_server_sleep_time_in_seconds'] = 1
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+            {
+                "instance_name": "irods_rule_engine_plugin-event_handler-data_object_modified-instance",
+                "plugin_name": "irods_rule_engine_plugin-event_handler-data_object_modified",
+                'plugin_specific_configuration': {
+                    "policies_to_invoke" : [
+                        {
+                            "conditional" : {
+
+                                "logical_path" : "\/tempZone.*",
+
+                                "metadata_exists" : {
+                                    "entity_type" : "user",
+                                    "attribute"   : "test_attribute",
+                                    "value"       : "test_value",
+                                    "units"       : "test_units",
+                                }
+                            },
+                            "active_policy_clauses" : ["post"],
+                            "events" : ["put", "get", "create", "read",
+                                        "write", "rename", "register", "unregister",
+                                        "replication", "checksum", "copy", "seek",
+                                        "truncate", "open", "close"],
+                            "policy"    : "irods_policy_testing_policy",
+                            "configuration" : {
+                            }
+                        }
+                    ]
+                }
+            }
+        )
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+           {
+                "instance_name": "irods_rule_engine_plugin-policy_engine-testing_policy-instance",
+                "plugin_name": "irods_rule_engine_plugin-policy_engine-testing_policy",
+                "plugin_specific_configuration": {
+                    "log_errors" : "true"
+                }
+           }
+        )
+
+        irods_config.commit(irods_config.server_config, irods_config.server_config_path)
+
+        IrodsController().restart()
+
+        try:
+            yield
+        finally:
+            pass
+
+@contextlib.contextmanager
+def event_handler_resource_metadata_exists(arg=None):
+    filename = paths.server_config_path()
+    with lib.file_backed_up(filename):
+        irods_config = IrodsConfig()
+        irods_config.server_config['advanced_settings']['rule_engine_server_sleep_time_in_seconds'] = 1
+
+        irods_config.server_config['plugin_configuration']['rule_engines'].insert(0,
+            {
+                "instance_name": "irods_rule_engine_plugin-event_handler-data_object_modified-instance",
+                "plugin_name": "irods_rule_engine_plugin-event_handler-data_object_modified",
+                'plugin_specific_configuration': {
+                    "policies_to_invoke" : [
+                        {
+                            "conditional" : {
+
+                                "logical_path" : "\/tempZone.*",
+
+                                "metadata_exists" : {
+                                    "entity_type" : "resource",
+                                    "attribute"   : "test_attribute",
+                                    "value"       : "test_value",
+                                    "units"       : "test_units",
+                                }
+                            },
+                            "active_policy_clauses" : ["post"],
+                            "events" : ["put", "get", "create", "read",
+                                        "write", "rename", "register", "unregister",
+                                        "replication", "checksum", "copy", "seek",
+                                        "truncate", "open", "close"],
                             "policy"    : "irods_policy_testing_policy",
                             "configuration" : {
                             }
@@ -178,6 +299,70 @@ class TestEventHandlerObjectModified(ResourceBase, unittest.TestCase):
     def tearDown(self):
         super(TestEventHandlerObjectModified, self).tearDown()
 
+    def test_event_handler_put_resource_metadata_exists(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            with event_handler_resource_metadata_exists():
+                try:
+                    admin_session.assert_icommand('imeta set -R demoResc test_attribute test_value test_units')
+
+                    filename = 'test_put_file'
+                    lib.create_local_testfile(filename)
+                    admin_session.assert_icommand('iput -f ' + filename)
+                    admin_session.assert_icommand('imeta ls -d ' + filename, 'STDOUT_SINGLELINE', 'PUT')
+                finally:
+                    admin_session.assert_icommand('irm -f ' + filename)
+                    admin_session.assert_icommand('imeta rm -R demoResc test_attribute test_value test_units')
+
+    def test_event_handler_put_resource_metadata_exists_fail(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            with event_handler_resource_metadata_exists():
+                try:
+                    filename = 'test_put_file'
+                    lib.create_local_testfile(filename)
+                    admin_session.assert_icommand('iput -f ' + filename)
+                    admin_session.assert_icommand('imeta ls -d ' + filename, 'STDOUT_SINGLELINE', 'None')
+                finally:
+                    admin_session.assert_icommand('irm -f ' + filename)
+
+    def test_event_handler_put_user_metadata_exists(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            with event_handler_user_metadata_exists():
+                try:
+                    admin_session.assert_icommand('imeta set -u rods test_attribute test_value test_units')
+
+                    filename = 'test_put_file'
+                    lib.create_local_testfile(filename)
+                    admin_session.assert_icommand('iput -f ' + filename)
+                    admin_session.assert_icommand('imeta ls -d ' + filename, 'STDOUT_SINGLELINE', 'PUT')
+                finally:
+                    admin_session.assert_icommand('irm -f ' + filename)
+                    admin_session.assert_icommand('imeta rm -u rods test_attribute test_value test_units')
+
+    def test_event_handler_put_user_metadata_exists_fail(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            with event_handler_user_metadata_exists():
+                try:
+                    filename = 'test_put_file'
+                    lib.create_local_testfile(filename)
+                    admin_session.assert_icommand('iput -f ' + filename)
+                    admin_session.assert_icommand('imeta ls -d ' + filename, 'STDOUT_SINGLELINE', 'None')
+                finally:
+                    admin_session.assert_icommand('irm -f ' + filename)
+
+    def test_event_handler_put_recursive_metadata_exists_fail(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            with event_handler_recurisve_collection_metadata_exists():
+                try:
+                    coll_name = 'test_collection_metadata'
+                    admin_session.assert_icommand('imkdir ' + coll_name)
+                    admin_session.assert_icommand('imeta add -C ' + coll_name + ' test_attribute_fail test_value_fail test_units_fail')
+
+                    filename = 'test_put_file'
+                    lib.create_local_testfile(filename)
+                    admin_session.assert_icommand('iput ' + filename + ' ' + coll_name)
+                    admin_session.assert_icommand('imeta ls -d ' + coll_name + '/' + filename, 'STDOUT_SINGLELINE', 'None')
+                finally:
+                    admin_session.assert_icommand('irm -rf ' + coll_name)
 
     def test_event_handler_put(self):
         with session.make_session_for_existing_admin() as admin_session:
